4c51886728208debcbe005d2abd1df783d6e2955
Sarah Rodrigues da Cruz <44931066895@>
2024-03-05 16:53:20 -0300

    atualizado

--- src/componentes/Lista.jsx
+++ src/componentes/Lista.jsx
@@ -1,9 +1,45 @@
-import { StyleSheet, View, Text } from "react-native"
+import { StyleSheet, View, FlatList } from "react-native"
+import { Usuario } from "./Usuario"
 
 export function Lista(){
+
+    const listaUsuarios =[
+        {
+            codigo: '1',
+            nome: 'João',
+            email: 'joao@gmail.com',
+            telefone: '(19) 98769-7975'
+        },
+
+        {
+            codigo: '2',
+            nome: 'Maria',
+            email: 'maria@gmail.com',
+            telefone: '(19) 98300-6547'
+        },
+
+        {
+            codigo: '3',
+            nome: 'Carlos',
+            email: 'carlos@gmail.com',
+            telefone: '(19) 99550-6767'
+        }
+
+    ]
+
     return(
-        <View style={estilos.texto}>
-            <Text style={estilos.texto}>Lista</Text>
+        <View>
+            <FlatList
+                data={listaUsuarios}
+                keyExtractor={ (item) => {item.codigo} }
+                renderItem={ ({item}) => (
+                    <Usuario 
+                        nome={item.nome}
+                        email={item.email}
+                        telefone={item.telefone}
+                    />
+                ) }
+            />
         </View>
     )
 }
---
+++ src/componentes/Usuario.jsx
@@ -0,0 +1,48 @@
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native"
+
+export function Usuario({nome, email, telefone}){
+    return(
+        <View style={estilos.conteiner}>
+            <View style={estilos.conteinerDados}>
+                <Text style={estilos.texto}>{nome}</Text>
+                <Text style={estilos.texto}>{email}</Text>
+                <Text style={estilos.texto}>{telefone}</Text>
+            </View>
+
+            <TouchableOpacity style={estilos.botao}>
+                <Text style={estilos.textoBotao}>-</Text>
+            </TouchableOpacity>
+
+        </View>
+    )
+}
+
+const estilos = StyleSheet.create({
+    conteiner: {
+        flexDirection: 'row',
+        margin: 5
+    },
+    conteinerDados: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    texto: {
+        color: '#000',
+        fontSize: 16
+    },
+    botao: {
+        width: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginStart: 10,
+        backgroundColor: '#f00',
+        borderRadius: 5,    
+    },
+    textoBotao: {
+        color: '#fff',
+        fontSize: 26
+    }
+})
